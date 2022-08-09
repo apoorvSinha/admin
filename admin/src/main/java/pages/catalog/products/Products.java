3d23878b3d9afe4e0a64fd14e6f8f40975ca81a1
apoorv <apoorvsinha96@gmail.com>
2022-08-09 10:47:14 +0530

    tr

--- admin/src/main/java/pages/catalog/products/Products.java
+++ admin/src/main/java/pages/catalog/products/Products.java
@@ -4,4 +4,23 @@ import base.TestBase;
 
 public class Products extends TestBase{
 	
+	public void addnew() {
+		
+	}
+	
+	public void downloadCatalogasPDF() {
+		
+	}
+	
+	public void export(String format, String number) {
+		
+	}
+	
+	public void importSheet() {
+		
+	}
+	
+	public void delete(String number) {
+		
+	}
 }
